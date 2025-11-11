6494c72c176dfac962b9356a7914c5c7c67cc09e
rafac183 <rafaelantoniocordero2002@gmail.com>
2025-11-11 20:48:05 -0300

    I created the purchase structure

--- src/products-inventory/products-inventory.module.ts
+++ src/products-inventory/products-inventory.module.ts
@@ -15,6 +15,8 @@ import { ItemsModule } from './stocks/items/items.module';
 import { LocationsModule } from './stocks/locations/locations.module';
 import { MovementsModule } from './stocks/movements/movements.module';
 import { Item } from './stocks/items/entities/item.entity';
+import { OrderModule } from './purchase/order/order.module';
+import { OrderItemModule } from './purchase/order-item/order-item.module';
 
 @Module({
   controllers: [ProductsInventoryController],
@@ -29,6 +31,8 @@ import { Item } from './stocks/items/entities/item.entity';
     ItemsModule,
     LocationsModule,
     MovementsModule,
+    OrderModule,
+    OrderItemModule,
   ],
   exports: [ProductsInventoryService, ProductsModule],
 })
